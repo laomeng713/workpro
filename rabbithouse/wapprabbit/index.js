910efb1f346c239c27883aa5c0b095444aa6ffba
mengxuchen <laomeng713@163.com>
2017-03-29 17:08:59 +0800

    上传两个底部固定标签页

--- rabbithouse/wapprabbit/index.js
+++ rabbithouse/wapprabbit/index.js
@@ -2,5 +2,7 @@
  * Created by Administrator on 2017/3/27.
  */
 $('#click_more').on('click',function(){
-    $('.sub_more')
+    console.log($(window).height());
+    $('.more_box').css("display","block");
+    $('.more_box').css("height",$(window).height());
 })
