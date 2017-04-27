148ce412aafa61245943da2b5a0047f6bfd3fbb4
mengxuchen <laomeng713@163.com>
2017-04-27 14:37:59 +0800

    更新摄影模板 pc端和移动端

---
+++ kids/assets/m/js/m_template.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Administrator on 2017/4/27.
+ */
+function phoneCheck($obj){
+    var $div = $obj.closest("div");
+    var uname = "", phone = "";
+    var regPhone = /^1[3578]\d{9}$/;
+    uname = $.trim($div.find("input[name='yname']").val());
+    phone = $.trim($div.find("input[name='ytel']").val());
+    if (uname == "") {
+        $div.find(".err_tips").text("姓名不能为空");
+        return false;
+    }
+    if (phone == '') {
+        $div.find(".err_tips").text("手机号不能为空");
+        return false;
+    }
+    if (!regPhone.test(phone)) {
+        $div.find(".err_tips").text("请填写正确的手机号码");
+        return false;
+    }else {
+        $div.find(".err_tips").text("");
+    }
+    $.ajax({
+        type:'post',
+        success:function(){
+            console.log('success');
+        },
+        error:function(){
+            console.log('error');
+        }
+    })
+}
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    autoplay: 3000,
+    pagination: '.swiper-pagination'
+})
