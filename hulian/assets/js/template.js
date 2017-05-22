aadfc7328c36ca081813b7d6d322e1d2f20c1eeb
mengxuchen <laomeng713@163.com>
2017-05-22 18:14:02 +0800

    更新摄影网页 18：13

--- hulian/assets/js/template.js
+++ hulian/assets/js/template.js
@@ -2,43 +2,39 @@
  * Created by Administrator on 2017/4/26.
  */
 
-var mySwiper = new Swiper('.swiper-container1',{
-    pagination : '.swiper-pagination',
-    autoplay : 5000,
-    paginationClickable :true,
+var mySwiper = new Swiper('.swiper-container1', {
+    pagination: '.swiper-pagination',
+    autoplay: 5000,
+    paginationClickable: true,
     direction: 'horizontal',
     autoHeight: true
 })
-var mySwiper1 = new Swiper('.chinacon',{
-    //slidesPerGroup : 4,
-    prevButton:'.swiper-button-prev',
-    nextButton:'.swiper-button-next',
-    loop : true,
-    slidesPerView : 'auto',
-    loopedSlides :4,
-    loopAdditionalSlides : 1
-//    spaceBetween : 20
-
+var mySwiper1 = new Swiper('.chinacon', {
+    prevButton: '.swiper-button-prev',
+    nextButton: '.swiper-button-next',
+    loop: true,
+    slidesPerView: 'auto',
+    loopedSlides: 4,
+    loopAdditionalSlides: 1
 })
-$('.success_info').hide();
 
-function phoneCheck($obj){ 
+function phoneCheck($obj) {
     var $div = $obj.closest("div");
-    var uname = "", phone = "",address='',message='',time='', sort='';
+    var uname = "", phone = "", address = '', message = '', time = '', sort = '';
     var regPhone = /^1[3578]\d{9}$/;
     uname = $.trim($div.find("input[name='uname']").val());
     sort = $.trim($div.find("input[name='sort']").val());
     phone = $.trim($div.find("input[name='phone']").val());
     address = $.trim($div.find("input[name='address']").val());
     message = $div.find("[name='message']").val();
-    time=$( "#date_time" ).datepicker( "getDate" );
+    time = $("#date_time").datepicker("getDate");
     console.log(time);
     console.log(message);
-    if(time==null){
+    if (time == null) {
         $div.find(".err_tips").text("请选择日期");
         return false;
     }
-    if(!$('.order_price input').is(':checked')){
+    if (!$('.order_price input').is(':checked')) {
         $div.find(".err_tips").text("请选择价格");
         return false;
     }
@@ -61,7 +57,7 @@ function phoneCheck($obj){
     if (message == "") {
         $div.find(".err_tips").text("留言不能为空");
         return false;
-    }else{
+    } else {
         $div.find(".err_tips").text("");
     }
 
@@ -72,93 +68,97 @@ function phoneCheck($obj){
         'mobile': phone,
         'address': address,
         'content': message,
-        '_csrf': $("#_csrf").val(),
+        '_csrf': $("#_csrf").val()
     };
     $.ajax({
-        async:false,
         type: 'POST',
         data: $data,
         url: window.signupUrl,// + '/jz-signup.html',
         dataType: "json",
-
-        success:function(data){
-            if(data.status != 200){
+        success: function (data) {
+            if (data.status != 200) {
                 $('.err_tips').text(data.message);
                 $('.err_tips').show();
             } else {
-            console.log('success');
+                console.log('success');
                 $('.err_tips').hide();
-            $('.order_form').hide();
-            $('.success_info').show();
-            $('.step_detail span').removeClass('active');
-            $('.step_detail span').eq(2).addClass('active');
-            $('#rInfos').html(data.rInfos);
-
+                $('.order_form').hide();
+                $('.success_info').show();
+                $('.step_detail span').removeClass('active').eq(2).addClass('active');
+                $('#rInfos').html(data.rInfos);
+                var mySwiper2 = new Swiper('.chinacon', {
+                    prevButton: '.swiper-button-prev',
+                    nextButton: '.swiper-button-next',
+                    loop: true,
+                    slidesPerView: 'auto',
+                    loopedSlides: 4,
+                    loopAdditionalSlides: 1,
+                    observer: true,//修改swiper自己或子元素时，自动初始化swiper
+                    observeParents: true//修改swiper的父元素时，自动初始化swiper
+                })
             }
         },
-        error:function(){
+        error: function () {
             console.log('error');
-            $('.order_form').hide();
-            $('.success_info').show();
-
 
         }
     })
 }
 
-$(window).bind('scroll',autoScroll);
-$(window).bind('scroll',slide_help);
-$(window).bind('scroll',fixedNav);
+$(window).bind('scroll', autoScroll);
+$(window).bind('scroll', slide_help);
+$(window).bind('scroll', fixedNav);
 
-function  autoScroll(){
-    var height=document.documentElement.scrollTop|| window.pageYOffset || document.body.scrollTop;
-    if(height>200){
+function autoScroll() {
+    var height = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (height > 200) {
         $('.collect_info').show();
-    }else{
+    } else {
         $('.collect_info').hide();
     }
 }
 //返回顶部
-function slide_help(){
-        var height=document.documentElement.scrollTop|| window.pageYOffset || document.body.scrollTop;
-        if(height>400){
-            $('.slide_help').show();
-        }else{
-            $('.slide_help').hide();
-        }
+function slide_help() {
+    var height = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (height > 400) {
+        $('.slide_help').show();
+    } else {
+        $('.slide_help').hide();
+    }
 }
-function fixedNav(){
-    var height=document.documentElement.scrollTop|| window.pageYOffset || document.body.scrollTop;
-    if(height>100){
+function fixedNav() {
+    var height = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    if (height > 100) {
         $('.topnav').addClass('fix');
         $('.hot_phone').show();
-    }else{
+    } else {
         $('.topnav').removeClass('fix');
         $('.hot_phone').hide();
     }
 }
 
-$('.order_style li').click(function(){
-    var index=$(this).index();
+$('.order_style li').click(function () {
+    var index = $(this).index();
     $(this).addClass('data-checked');
     $('.order_style').hide();
     $('.order_form').show();
-    $('.step_detail span').removeClass('active');
-    $('.step_detail span').eq(1).addClass('active');
+    $('.step_detail span').removeClass('active').eq(1).addClass('active');
     $('.order_form .order_price').hide();
     $('.order_form .order_price').eq(index).show();
 
 
 })
-$('.order_price label').click(function(){
-  $(this).children().css('opacity','1');
-   $(this).siblings('input').prop('checked','true');
-  $(this).parent().siblings('li').children('label').children('i').css('opacity','0');
+$('.order_price label').click(function () {
+    $(this).children().css('opacity', '1');
+    $(this).siblings('input').prop('checked', 'true');
+    $(this).parent().siblings('li').children('label').children('i').css('opacity', '0');
 })
-$('.order_pre').click(function(){
+$('.order_pre').click(function () {
     $('.order_form').hide();
     $('.order_style').show();
-    $('.step_detail span').removeClass('active');
-    $('.step_detail span').eq(0).addClass('active');
+    $('.step_detail span').removeClass('active').eq(0).addClass('active');
+
 })
+
+
 
